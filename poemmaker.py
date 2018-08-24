df96ba53568411e0a451682919c0296056a11f60
ke-zhang-rd <kz2249@columbia.edu>
2018-08-24 10:34:38 -0400

    add a temple file

---
+++ poemmaker.py
@@ -0,0 +1,14 @@
+import os.path
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        sefl.render('index.html')
+
